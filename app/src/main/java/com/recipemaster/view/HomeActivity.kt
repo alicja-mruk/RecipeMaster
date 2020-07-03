9e5299d015f68b428eea5d52267c9d8f810384f4
alicja.99 <alicja.mruk99@gmail.com>
2020-07-03 17:51:28 +0200

    init

---
+++ app/src/main/java/com/recipemaster/view/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.recipemaster.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.recipemaster.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
