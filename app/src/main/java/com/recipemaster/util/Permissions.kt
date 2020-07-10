3fb8ce5fa828c8d11f931771856ea43e29113a24
alicja.99 <alicja.mruk99@gmail.com>
2020-07-10 21:06:52 +0200

    toast change

--- app/src/main/java/com/recipemaster/util/Permissions.kt
+++ app/src/main/java/com/recipemaster/util/Permissions.kt
@@ -2,11 +2,12 @@ package com.recipemaster.util
 
 import android.Manifest
 
-object Permissions{
+object Permissions {
 
     val storagePermissions = listOf(
         Manifest.permission.WRITE_EXTERNAL_STORAGE,
         Manifest.permission.READ_EXTERNAL_STORAGE
     )
-    val facebookCallPermission =  listOf("public_profile")
+
+    val facebookCallPermission = listOf("public_profile")
 }
